5700f8dd6e190d55c41870dbe56b6790b6997a26
iamsunny619 <iamsunny619@gmail.com>
2025-08-11 23:18:42 +0530

    making new branch for leaning

--- python/basic/condition/basic.py
+++
@@ -1,9 +0,0 @@
-currentAge = int(input('enter your age: '))
-age =18
-
-if currentAge >= age:
-    print('Your Age',currentAge," is greater than or equal to",age)
-elif currentAge < age:
-    print('Your Age',currentAge," is less than",age)
-else:
-    print('Your Age',currentAge," is same as",age)
