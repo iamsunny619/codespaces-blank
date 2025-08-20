d885fe83e87fe5c3166dafcf444c422d5d1da01f
iamsunny619 <60790066+iamsunny619@users.noreply.github.com>
2025-08-20 18:58:50 +0000

    loops added

---
+++ python/basic/loops/forLoop/forLoopBasic.py
@@ -0,0 +1,6 @@
+for a in range(1,5):
+   print(a)
+print('---------------------------------------')
+for i in range(1,10,2):
+    print(i)
+
---
+++ python/moderate/Lambda/lambdaBasic.py
@@ -0,0 +1,9 @@
+squared = lambda num : num * num
+
+print(squared(3))
+
+multiply = lambda a,b : (a+b)*(a+b)
+
+print(multiply(1,1))
+
+
