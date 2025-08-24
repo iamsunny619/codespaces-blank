33a3553803a3c269c082f28d21dc125a88385624
iamsunny619 <60790066+iamsunny619@users.noreply.github.com>
2025-08-24 19:13:22 +0000

    new list example added

---
+++ python/moderate/list/examples/01.py
@@ -0,0 +1,36 @@
+num = [10,20,30,40,50,60]
+'''print(num[0]) #10
+print(num[-1]) #60
+print(num[1:4])#20,30,40
+print(num[::-1]) # 60,50,....,10 '''
+
+#########################################################
+
+fruits =[]
+fruits.append('apple')
+fruits.append('banana')
+fruits.append('cherry')
+''' print(fruits)  #['apple', 'banana', 'cherry']
+
+fruits.insert(1,'orange')
+print(fruits) #['apple', 'orange', 'banana', 'cherry']
+
+fruits.remove('banana')
+print(fruits) #['apple', 'orange', 'cherry']
+
+print(fruits.pop())
+print(fruits) #['apple', 'orange'] '''
+
+########################################################
+
+cars = ['bmw','honda','tata','bmw','vw']
+'''
+print(cars.index('tata'))
+print(cars.count('bmw'))
+cars[1]='huyndai'
+if cars.__contains__('tesla'):
+    print("yes")
+else:
+    print('NO')
+'''
+#######################################
