28f79f435c6d76d3cb2d2dde0ad5ba4b7f4467f2
iamsunny619 <iamsunny619@gmail.com>
2025-08-24 22:54:03 +0530

    new structre for list folder

--- python/moderate/list/listCopy.py
+++ python/moderate/list/listCopy.py
@@ -1,6 +1,9 @@
+from copy import deepcopy
 cars = ['BMW', 'HONDA', 'VW', 'TATA']
 carsTemp= cars
 carCopyS = cars.copy()
+
+deepCars = deepcopy(cars)
 
 print(id(cars) == id(carCopyS)) # false because, it is shallow copy where it is a new list, but the element have same reference
 print(id(carsTemp) == id(cars)) ##true beacuse, it is refernce copy
@@ -9,4 +12,6 @@ cars[2] = 'VolksWogan'
 print(cars)
 print(carsTemp)
 print(carCopyS)
+print(deepCars)
+
 
